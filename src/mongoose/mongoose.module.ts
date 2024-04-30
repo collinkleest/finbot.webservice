d400e36c797604827887d9822402c05a55570de8
Collin Kleest <collinkleest@gmail.com>
2024-04-30 10:50:26 -0400

    mongo db setup

--- src/mongoose/mongoose.module.ts
+++ src/mongoose/mongoose.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot(process?.env?.NODE_ENV === 'prod' ? 
-  'mongodb://172.20.0.2/finbot' : 'mongodb://localhost:27017/finbot'
+  imports: [MongooseModule.forRoot(
+    `mongodb+srv://finbot:89DVUcRSX6O1p3ao@atlascluster.tkvfoe0.mongodb.net/`
   )],
 })
 export class MongooseConfigModule {}
